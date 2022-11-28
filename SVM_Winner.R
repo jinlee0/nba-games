57e0dad80ab70fbd7534b24db159377111a72f77
jinlee0 <wls4370@gmail.com>
2022-11-28 18:24:59 +0900

    refactor: define formula and use in svm()

--- SVM_Winner.R
+++ SVM_Winner.R
@@ -17,7 +17,6 @@ head(games.df)
 
 # 정규화
 library("caret")
-columns = c('FG_PCT_home', 'FT_PCT_home', 'FG3_PCT_home', 'AST_home', 'REB_home', 'FG_PCT_away', 'FT_PCT_away', 'FG3_PCT_away', 'AST_away', 'REB_away')
 games.df = predict(preProcess(x = games.df[c(8:13, 15:20)], method = c("range")), games.df)
 head(games.df)
 
@@ -30,15 +29,15 @@ reg = lm(HOME_TEAM_WINS~.,
 options(scipen = 999)
 summary(reg)
 
-selected.columns = c('FG_PCT_home', 'FG3_PCT_home', 'AST_home', 'FG_PCT_away', 'FG3_PCT_away', 'AST_away', 'HOME_TEAM_WINS')
-games.df = games.df[selected.columns]
-
+# SVM에서 종속변수, 독립변수에 대한 formula를 정의한다.
+#formula.svm = HOME_TEAM_WINS~FG_PCT_home+FG3_PCT_home+AST_home+FG_PCT_away+FG3_PCT_away+AST_away
+formula.svm = HOME_TEAM_WINS~FG_PCT_home+FG3_PCT_home+FG_PCT_away+FG3_PCT_away
 games.df$HOME_TEAM_WINS = factor(games.df$HOME_TEAM_WINS)
 
 # gamma, cost 최적값 찾기기
 # 굉장히 오래 걸리기 때문에 무작위 1000개 데이터로 해본다.
 # 실제 모델을 만들 때에는 전체 데이터를 활용해 좀 더 정확한 값을 찾아야 한다.
-games.best.model = tune.svm(HOME_TEAM_WINS~.,
+games.best.model = tune.svm(formula.svm,
          data = games.df[sample(1:nrow(games.df), size = 1000),],
          gamma = 2^(-1:1),
          cost = 2^(2:4))
@@ -49,7 +48,7 @@ train.df = games.df[train.index,]
 test.df = games.df[-train.index, ]
 
 # 트레이닝 데이터 학습. 오래 걸린다. 2분 정도.
-train.model = svm(HOME_TEAM_WINS~.,
+train.model = svm(formula.svm,
     data = train.df,
     gamma = games.best.model$best.parameters$gamma,
     cost = games.best.model$best.parameters$cost)
@@ -66,10 +65,7 @@ test.pred.table
 Epi::ROC(test=test.pred.list, stat=test.df$HOME_TEAM_WINS, plot='ROC', AUC=T, main="SVM")
 
 # 모델 완성하기. 역시 오래 걸린다.
-# model = svm(HOME_TEAM_WINS~
-#                     FG_PCT_home+FT_PCT_home+FG3_PCT_home+
-#                     AST_home+REB_home+FG_PCT_away+FT_PCT_away+
-#                     FG3_PCT_away+AST_away+REB_away,
+# model = svm(formula.svm,
 #                   data = games.df,
 #                   gamma = games.best.model$best.parameters$gamma,
 #                   cost = games.best.model$best.parameters$cost)
